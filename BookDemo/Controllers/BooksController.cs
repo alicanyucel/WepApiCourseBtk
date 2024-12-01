18bfda2b3e93d60418a944343234ea694d67968d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-01 14:31:56 +0300

    Modellerle calisma

---
+++ BookDemo/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationDbContext.Books;
+            return Ok (books);
+        }
+        [HttpGet]
+    }
+}
---
+++ BookDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookDemo.Models;
+namespace BookDemo;
+
+public static class ApplicationDbContext
+{
+    public static List<Book> Books { get; set; }
+    static ApplicationDbContext()
+    {
+        Books = new List<Book>()
+        {
+            new Book {Id=1,Title="Karagöz ve Hacivat",Price=500},
+            new Book {Id=2,Title="Suc ve Ceza",Price=400 },
+            new Book {Id=3,Title="Atatürk ve Cumhuriyet",Price=620 }
+    };
+    }
+}
