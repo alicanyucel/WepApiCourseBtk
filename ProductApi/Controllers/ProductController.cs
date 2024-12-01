087415825d00479e84178d3f167ff094d67efafe
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-01 13:16:32 +0300

    logllama

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
+using System.Reflection.Metadata.Ecma335;
+
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+
+            var products = new List<Product>()
+            {
+            new Product(){Id=1,Name="Bilgisayar"},
+            new Product(){Id=2,Name="Telefon"},
+            new Product(){Id=3,Name="Mouse"}
+
+             };
+            _logger.LogInformation("GetAllProducts action has been called");
+            return Ok(products);
+
+        }
+        [HttpPost]
+        public IActionResult GetAllProducts([FromBody] Product product)
+        {
+
+            _logger.LogWarning("Product has been created");
+            return Ok(product);
+
+        }
+    }
+}
---
+++ ProductApi/Program.cs
@@ -0,0 +1,36 @@
+namespace ProductApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Logging.ClearProviders(); // default loglama iocden kurtarýyor 
+            builder.Logging.AddConsole(); // console logla
+            builder.Logging.AddDebug(); // debug a logla
+            var app = builder.Build();
+
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
