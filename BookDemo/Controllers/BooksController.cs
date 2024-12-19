f6d2d8e253d7495f7d36606d2cfedf3838d150c1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-19 12:11:58 +0300

    put

--- BookDemo/Controllers/BooksController.cs
+++ BookDemo/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookDemo.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BookDemo.Controllers
 {
@@ -12,17 +14,57 @@ namespace BookDemo.Controllers
         public IActionResult GetAllBooks()
         {
             var books = ApplicationDbContext.Books;
-            return Ok (books);
+            return Ok(books);
         }
         [HttpGet("{id:int}")]
-        public IActionResult GetOneBook([FromRoute(Name ="id")]int id)
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
         {
-            var book=ApplicationDbContext.Books.Where(b=>b.Id.Equals(id)).SingleOrDefault();
-            if(book is null)
+            var book = ApplicationDbContext.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+            if (book is null)
             {
                 return NotFound();
             }
-            return Ok (book);
+            return Ok(book);
         }
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                    return BadRequest();
+                ApplicationDbContext.Books.Add(book);
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateBookById([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            //CHECK BOOK
+            var entity = ApplicationDbContext.Books.Find(b=>book.Id==book.Id);
+            if (entity is null)
+            {
+                return NotFound(); // 400
+            }
+            // CHECK ID
+            if (id != book.Id)
+            {
+                return BadRequest("ivalid argumnet");// 404
+            }
+            ApplicationDbContext.Books.Remove(entity);
+            book.Id = entity.Id;
+            // added to List
+            ApplicationDbContext.Books.Add(book);
+
+            return Ok(book);
+        }
+       
+
+
     }
 }
