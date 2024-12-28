0f3bd34f68d138639869f207b0ee221d4eb14b7d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-28 13:34:02 +0300

    ef core intro

---
+++ BookStoreApp/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            return Ok();
+        }
+    }
+}
